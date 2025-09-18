fcea5f48127829e9731ed31b09a2c4b6a299be83
Pradeep-G-A <gapradeep123@gmail.com>
2025-09-19 01:08:41 +0530

    deleted unwanted files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Routes,Route } from "react-router-dom";
 import BackgroundAnimation from "./BackgroundAnimation";
 import Portfolio from "./portfolio/portfolio";
-import Content from "./portfolio/content";
 function App() {
   return(
     <>
     <BackgroundAnimation/>
     <Routes>
       <Route path="/" element={<Portfolio/>}></Route>
-      <Route path="/content" element={<Content/>}></Route>
     </Routes>
     </>
   );
